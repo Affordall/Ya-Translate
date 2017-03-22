5f66748b87df2fbac478b4e05bcd9722a4a42982
Vladimir Degtev <->
2017-03-22 21:58:30 +0300

    Added colors and basic activity, new App class

---
+++ app/src/main/java/realjamapps/com/yatranslate/application/YaApp.java
@@ -0,0 +1,15 @@
+package realjamapps.com.yatranslate.application;
+
+import android.app.Application;
+
+public class YaApp extends Application {
+
+    public YaApp() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
